e7a76a29363ce63f7d10cc70f3de5086e9196cd8
dohongthinh <dohongthinhdht1999@gmail.com>
2020-02-22 10:50:40 -0500

    Finish basci function

--- Scripts/scenes/Play.js
+++ Scripts/scenes/Play.js
@@ -16,23 +16,165 @@ var scenes;
 (function (scenes) {
     var Play = /** @class */ (function (_super) {
         __extends(Play, _super);
-        // PRIVATE INSTANCE MEMBERS
         // PUBLIC PROPERTIES
         // CONSTRUCTOR
         function Play() {
             var _this = _super.call(this) || this;
+            _this._finalResult = 0;
             _this.Start();
             return _this;
         }
         // PRIVATE METHODS
+        Play.prototype.DisplayResult = function () {
+            this.removeChild(this._resultLabel);
+            this._resultLabel = new objects.Label("Result: " + this._finalResult, "25px", "Consolas", "#000000", 320, 50, true);
+            this.addChild(this._resultLabel);
+        };
+        Play.prototype.GetDiceResult = function () {
+            var dice1Result = util.Mathf.RandomRange(1, 5);
+            var dice2Result = util.Mathf.RandomRange(1, 5);
+            switch (Math.round(dice1Result)) {
+                case 1:
+                    {
+                        this._finalResult += 1;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("1"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                case 2:
+                    {
+                        this._finalResult += 2;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("2"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                case 3:
+                    {
+                        this._finalResult += 3;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("3"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                case 4:
+                    {
+                        this._finalResult += 4;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("4"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                case 5:
+                    {
+                        this._finalResult += 5;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("5"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                case 6:
+                    {
+                        this._finalResult += 6;
+                        this.removeChild(this._dice1);
+                        this._dice1 = new objects.Button(config.Game.ASSETS.getResult("6"), 200, 200, true);
+                        this.addChild(this._dice1);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            ;
+            switch (Math.round(dice2Result)) {
+                case 1:
+                    {
+                        this._finalResult += 1;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("1"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 1:
+                    {
+                        this._finalResult += 1;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("1"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 2:
+                    {
+                        this._finalResult += 2;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("2"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 3:
+                    {
+                        this._finalResult += 3;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("3"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 4:
+                    {
+                        this._finalResult += 4;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("4"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 5:
+                    {
+                        this._finalResult += 5;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("5"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                case 6:
+                    {
+                        this._finalResult += 6;
+                        this.removeChild(this._dice2);
+                        this._dice2 = new objects.Button(config.Game.ASSETS.getResult("6"), 450, 200, true);
+                        this.addChild(this._dice2);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            ;
+            console.log("rolled");
+            this.DisplayResult();
+            this._finalResult = 0;
+        };
         // PUBLIC METHODS
         //initialize and instatiate
         Play.prototype.Start = function () {
+            //instantiate a new Text object
+            this._resultLabel = new objects.Label("Result: ", "25px", "Consolas", "#000000", 320, 50, true);
+            // buttons
+            this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
+            this._dice1 = new objects.Button(config.Game.ASSETS.getResult("blank"), 200, 200, true);
+            this._dice2 = new objects.Button(config.Game.ASSETS.getResult("blank"), 450, 200, true);
             this.Main();
         };
         Play.prototype.Update = function () {
         };
         Play.prototype.Main = function () {
+            var _this = this;
+            this.addChild(this._resultLabel);
+            this.addChild(this._rollButton);
+            this.addChild(this._dice1);
+            this.addChild(this._dice2);
+            this._rollButton.on("click", function () { _this.GetDiceResult(); });
         };
         return Play;
     }(objects.Scene));
--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -3,8 +3,14 @@ module scenes
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
- 
+        private _resultLabel: objects.Label;
+        private _rollButton: objects.Button;
+        private _dice1: objects.Button;
+        private _dice2: objects.Button;
 
+        private _outcome:String;
+        private _result:number;
+        private _finalResult = 0;
         // PUBLIC PROPERTIES
 
         // CONSTRUCTOR
@@ -16,14 +22,157 @@ module scenes
         }
 
         // PRIVATE METHODS
+        private DisplayResult():void
+        {
+            this.removeChild(this._resultLabel);
+            this._resultLabel=new objects.Label("Result: "+this._finalResult, "25px", "Consolas", "#000000", 320, 50 , true);
+            this.addChild(this._resultLabel);
+        }
 
+        private GetDiceResult():void
+        {
+            let dice1Result = util.Mathf.RandomRange(1,5);
+            let dice2Result = util.Mathf.RandomRange(1,5);
+            switch(Math.round(dice1Result))
+            {
+                case 1:
+                {
+                    this._finalResult += 1;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("1"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                case 2:
+                {
+                    this._finalResult += 2;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("2"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                case 3:
+                {
+                    this._finalResult += 3;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("3"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                case 4:
+                {
+                    this._finalResult += 4;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("4"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                case 5:
+                {
+                    this._finalResult += 5;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("5"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                case 6:
+                {
+                    this._finalResult += 6;
+                    this.removeChild(this._dice1);
+                    this._dice1 = new objects.Button(config.Game.ASSETS.getResult("6"), 200, 200, true);
+                    this.addChild(this._dice1);
+                    break;
+                }
+                default:
+                    {
+                        break;
+                    }
+
+            };
+
+            switch(Math.round(dice2Result))
+            {
+                case 1:
+                {
+                    this._finalResult += 1;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("1"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                    
+                case 1:
+                {
+                    this._finalResult += 1;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("1"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                case 2:
+                {
+                    this._finalResult += 2;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("2"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                case 3:
+                {
+                    this._finalResult += 3;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("3"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                case 4:
+                {
+                    this._finalResult += 4;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("4"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                case 5:
+                {
+                    this._finalResult += 5;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("5"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                case 6:
+                {
+                    this._finalResult += 6;
+                    this.removeChild(this._dice2);
+                    this._dice2 = new objects.Button(config.Game.ASSETS.getResult("6"), 450, 200, true);
+                    this.addChild(this._dice2);
+                    break;
+                }
+                default:
+                    {
+                        break;
+                    }
+
+
+            };
+            console.log("rolled");
+            this.DisplayResult();
+            this._finalResult=0;
+        }
         // PUBLIC METHODS
+
 
         //initialize and instatiate
         public Start(): void 
         {
             
-
+            //instantiate a new Text object
+            this._resultLabel = new objects.Label("Result: ", "25px", "Consolas", "#000000", 320, 50 , true);
+            // buttons
+            this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
+            this._dice1 = new objects.Button(config.Game.ASSETS.getResult("blank"), 200, 200, true);
+            this._dice2 = new objects.Button(config.Game.ASSETS.getResult("blank"), 450, 200, true);
             
              this.Main();
         }        
@@ -35,8 +184,15 @@ module scenes
         
         public Main(): void 
         {
+     
+            this.addChild(this._resultLabel);
 
+        
+            this.addChild(this._rollButton);
+            this.addChild(this._dice1);
+            this.addChild(this._dice2);
 
+            this._rollButton.on("click", ()=>{this.GetDiceResult()});
         }
 
         
